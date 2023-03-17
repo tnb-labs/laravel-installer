b31afe1653f6640567833a980344c316610f3f2b
耗子 <i@haozi.net>
2023-03-18 00:05:14 +0800

    Add alreadyUpdated redirect.

--- src/Middleware/canUpdate.php
+++ src/Middleware/canUpdate.php
@@ -29,7 +29,32 @@ class canUpdate
                 }
 
                 if ($this->alreadyUpdated()) {
-                    abort(404);
+                    $installedRedirect = config('installer.installedAlreadyAction');
+
+                    switch ($installedRedirect) {
+
+                        case 'route':
+                            $routeName = config('installer.installed.redirectOptions.route.name');
+                            $data = config('installer.installed.redirectOptions.route.message');
+
+                            return redirect()->route($routeName)->with(['data' => $data]);
+                            break;
+
+                        case 'abort':
+                            abort(config('installer.installed.redirectOptions.abort.type'));
+                            break;
+
+                        case 'dump':
+                            $dump = config('installer.installed.redirectOptions.dump.data');
+                            dd($dump);
+                            break;
+
+                        case '404':
+                        case 'default':
+                        default:
+                            abort(404);
+                            break;
+                    }
                 }
                 break;
 
